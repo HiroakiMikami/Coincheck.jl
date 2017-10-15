430f0cce83bbc61b12b3b3833a87305af3ae20e8
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2017-10-15 16:11:49 +0900

    Merge call_private_api and call_public_api
      ref #9

--- src/Coincheck.jl
+++ src/Coincheck.jl
@@ -10,34 +10,31 @@ include("common.jl")
 
 include("HttpUtil.jl")
 
-export call_public_api
-function call_public_api(path, args = Nullable())
-    call_public_api(default_http_client, path, args)
+function call_http_api(path; method = Methods.GET, credential = Nullable(), args = Dict())
+    call_http_api(defaut_http_client, path, method = method, credential = credential, args = args)
 end
-function call_public_api(client :: HttpClient, path, args = Nullable())
-    response = HttpUtil.make_http_request(Methods.GET, HttpUtil.convert_to_url(client, path, args))
-    JSON.parse(response.body)
-end
-
-export call_private_api
-function call_private_api(credential, method, path, args = Nullable())
-    call_private_api(default_http_client, credential, method, path, args)
-end
-function call_private_api(client :: HttpClient, credential, method, path, args = Nullable())
-    # nonce
-    nonce = string(UInt64(Dates.time() * 1e6))
-    # url
+function call_http_api(client:: HttpClient, path; method = Methods.GET, credential = Nullable(), args = Dict())
+    # URL
     url = HttpUtil.convert_to_url(client, path, (method == Methods.GET) ? args : Nullable())
-    # body
-    body = (method == Methods.GET) ? "" : JSON.json(args)
+    # Body
+    body = (method == Methods.GET) ? Nullable() : JSON.json(args)
 
-    message = nonce * url * body
-    signature = Nettle.hexdigest("sha256", credential.secret_key, message)
+    headers = Nullable()
 
-    headers = Dict{String, String}("ACCESS-KEY" => credential.access_key, "ACCESS-NONCE" => nonce, "ACCESS-SIGNATURE" => signature)
-    response = (body == "") ? HttpUtil.make_http_request(method, url, headers = headers) : HttpUtil.make_http_request(method, url, headers = headers, body = body)
+    if !isnull(credential)
+        # nonce
+        nonce = string(UInt64(Dates.time() * 1e6))
+
+        message = nonce * url * body
+        signature = Nettle.hexdigest("sha256", credential.secret_key, message)
+
+        headers = Dict{String, String}("ACCESS-KEY" => credential.access_key, "ACCESS-NONCE" => nonce, "ACCESS-SIGNATURE" => signature)
+    end
+
+    response = HttpUtil.make_http_request(method, url, headers = headers, body = body)
     return JSON.parse(response.body)
 end
+export call_http_api
 
 on_text(handler:: WebSocketClient, s:: String) = begin
     try
